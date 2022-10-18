1018d7bb16f0513326115ced29c7a7cc27a10af6
Sean Rice <sean.m.rice88@gmail.com>
2022-10-18 09:33:09 -0500

    Created the solution and project

---
+++ DungeonApplication/Dungeon/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Dungeon
+{
+    internal class Program
+    {
+        //SVM tab tab creates a static void Main()
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
