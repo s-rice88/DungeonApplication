3eefe71da31a7620c4fb2f9806d5befcd784d48d
Sean Rice <sean.m.rice88@gmail.com>
2022-10-19 11:15:54 -0500

    set up custom datatypes and finished

---
+++ DungeonApplication/DungeonLibrary/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Weapon
+    {
+
+        //minDamage – int
+
+        //maxDamage – int
+
+        //name – string
+
+        //bonusHitChance – int
+
+        //isTwoHanded - bool
+
+        //Full Qualified CTOR
+
+        //Nicely Formatted ToString()
+
+
+    }
+}
